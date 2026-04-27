1b28cad14a5dd85a2d56d2330a9a272d9a2623ad
sans-algo <sanskar.pawar2003@gmail.com>
2026-04-27 17:51:43 +0530

    fix: add ssl for postgres

--- backend/migrations/01_create_database.js
+++ backend/migrations/01_create_database.js
@@ -8,9 +8,12 @@ const { Client } = pkg;
 const client = new Client({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: "postgres",
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const queries = [`CREATE DATABASE ${process.env.DB_NAME}`];
