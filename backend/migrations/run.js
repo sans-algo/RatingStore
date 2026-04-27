fe884a99ee116f8deb9d47dde06795106ade7a34
sans-algo <sanskar.pawar2003@gmail.com>
2026-04-27 17:58:22 +0530

    fix the database name

--- backend/migrations/run.js
+++ backend/migrations/run.js
@@ -11,7 +11,7 @@ async function runMigrations() {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "postgres",
+    database: process.env.DB_NAME,
   });
 
   try {
